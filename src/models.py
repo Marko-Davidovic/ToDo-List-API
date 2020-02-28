7a6996d1d7e3d66e9a3734f3e5a941694ce7ca4e
Marko Davidovic <davidovic-m@hotmail.com>
2020-02-28 21:49:56 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -2,16 +2,38 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=True)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    name = db.Column(db.String(120), unique=False, nullable=True)
+    todos = db.relationship('Todos', backref='user', lazy=True)
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __repr__(self):
+        return '<User %r>' % self.username
+    def serialize_todos(self):
+        return list(map(lambda x: x.serialize(), self.todos))
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            "username": self.username,
+            "email": self.email,
+            "name": self.name,
+             "todos": list(map(lambda x: x.serialize(), self.todos))
+        }
+
+class Todos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(250), nullable=False)
+    done = db.Column(db.Boolean, unique=False, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+    def __repr__(self):
+        return '<Todo for user with ID %r>' % self.user_id
+
+    def serialize(self):
+        return dict(
+            label = self.label,
+            done = self.done
+        )
